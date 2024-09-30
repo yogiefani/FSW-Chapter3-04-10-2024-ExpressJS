62d5ea8b31358d84d9f55d30be08d7f66753adc7
yogiefani <yogiefaniyc@gmail.com>
2024-09-30 22:08:30 +0700

    feat: add server js use express js

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Hello FSW2!")
---
+++ server.js
@@ -0,0 +1,34 @@
+const fs = require("fs")
+// const http = require("http")
+const express = require("express")
+
+const app = express();
+
+// default = healt check
+app.get("/", (req, res) => {
+    res.status(200).json({
+        "status" : "Success",
+        "message" : "Application is running good.."
+    })
+})
+app.get("/yogi", (req, res) => {
+    res.status(200).json({
+        "status": "Success",
+        "message": "Ping Successfully !"
+    })
+})
+
+//middleware / handler untuk url yang tidak dapat diakses karena memang tidak ada di aplikasi   
+// membuat middleware = our own middleware
+app.use((req,res, next) => {
+    res.status(404).json({
+        "status": "Failed",
+        "Message": "API not exist !!"
+    })
+})
+
+
+
+app.listen("3000", () =>{
+    console.log("start aplikasi di port 3000")
+})
